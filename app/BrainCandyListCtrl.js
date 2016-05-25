b3c6554e1b063d9ee3aac63f7b9e457b7538cd09
Tomkorr <tomkor1992@gmail.com>
2016-05-25 10:32:08 +0200

    Update BrainCandyListCtrl.js

--- app/BrainCandyListCtrl.js
+++ app/BrainCandyListCtrl.js
@@ -4,7 +4,9 @@
       function BrainCandyListCtrl(CandyDAO)
       {
             var ctrl = this;
-            var sentence = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat';
+            var sentence = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'+
+                'incididunt ut labore et dolore magna aliqua.'+
+                'Ut enim ad quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat';
             
             if (CandyDAO != undefined) {
                   CandyDAO.query ().then (function (data)
